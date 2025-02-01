4fc42001300c2feb3a828e0697e0e8bdd1561cb0
bluegreensea <wfzhang0511@gmail.com>
2025-02-01 23:40:00 +0800

    Remove unneeded repeat open Inventory

--- velocity/src/main/kotlin/bluesea/aquautils/PluginListener.kt
+++ velocity/src/main/kotlin/bluesea/aquautils/PluginListener.kt
@@ -8,15 +8,12 @@ import com.velocitypowered.api.event.Subscribe
 import com.velocitypowered.api.event.connection.DisconnectEvent
 import com.velocitypowered.api.event.player.PlayerChatEvent
 import com.velocitypowered.api.event.player.ServerPostConnectEvent
-import com.velocitypowered.api.scheduler.ScheduledTask
 import java.util.UUID
-import java.util.concurrent.TimeUnit
 import net.kyori.adventure.audience.Audience
 import net.kyori.adventure.text.Component
 
 class PluginListener(private val plugin: AquaUtilsVelocity) {
     private val lobbyPlayer = HashMap<UUID, Int>()
-    private val scheduledTasks = HashMap<UUID, ScheduledTask>()
 
     @Subscribe
     fun onPlayerChat(event: PlayerChatEvent) {
@@ -36,34 +33,16 @@ class PluginListener(private val plugin: AquaUtilsVelocity) {
     @Suppress("UnstableApiUsage")
     @Subscribe
     fun onPlayerServerConnect(event: ServerPostConnectEvent) {
+        val player = event.player
         if (plugin.velocityGUI != null && plugin.velocityGUI is VelocityGUI) {
-            val player = event.player
-            if (player.currentServer.get().serverInfo.name == "minigame") {
-                lobbyPlayer[player.uniqueId] = 1
-                scheduledTasks[player.uniqueId] = plugin.proxyServer.scheduler
-                    .buildTask(plugin) { it: ScheduledTask ->
-                        try {
-                            if (player.currentServer.isPresent && player.currentServer.get().serverInfo.name == "minigame") {
-                                InventoryLauncher(plugin.velocityGUI).execute("servers", player)
-                                lobbyPlayer[player.uniqueId] = lobbyPlayer[player.uniqueId]!! + 1
-                                if (lobbyPlayer[player.uniqueId]!! > 2) {
-                                    scheduledTasks.remove(player.uniqueId)
-                                    it.cancel()
-                                }
-                            } else {
-                                scheduledTasks.remove(player.uniqueId)
-                                it.cancel()
-                            }
-                        } catch (e: Exception) {
-                            e.printStackTrace()
-                            scheduledTasks.remove(player.uniqueId)
-                            it.cancel()
-                        }
+            player.currentServer.ifPresent { currentServer ->
+                if (currentServer.serverInfo.name == "minigame") {
+                    try {
+                        InventoryLauncher(plugin.velocityGUI).execute("servers", player)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
                     }
-                    .repeat(3L, TimeUnit.SECONDS)
-                    .schedule()
-            } else {
-                lobbyPlayer.remove(player.uniqueId)
+                }
             }
         }
     }
@@ -72,9 +51,5 @@ class PluginListener(private val plugin: AquaUtilsVelocity) {
     fun onPlayerDisconnect(event: DisconnectEvent) {
         val player = event.player
         lobbyPlayer.remove(player.uniqueId)
-        if (scheduledTasks[player.uniqueId] != null) {
-            scheduledTasks[player.uniqueId]!!.cancel()
-            scheduledTasks.remove(player.uniqueId)
-        }
     }
 }
